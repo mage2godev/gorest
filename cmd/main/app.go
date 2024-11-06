d0be1789b893ab1a85c6033bec3c7c44ab5a694a
Andrew <andrew@example.com>
2024-11-06 17:56:50 +0200

    add simple service

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net"
+	"net/http"
+	"time"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	name := params.ByName("name")
+	w.Write([]byte(fmt.Sprintf("Hello %s", name)))
+}
+func main() {
+	router := httprouter.New()
+	router.GET("/:name", IndexHandler)
+
+	listener, err := net.Listen("tcp", "127.0.0.1:1234")
+
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatalln(server.Serve(listener))
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module awesomeProject
 go 1.22
 
 require github.com/mitchellh/mapstructure v1.5.0
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
